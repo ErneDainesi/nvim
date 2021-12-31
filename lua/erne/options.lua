fc3000e9d048322e93324441232ec6666dbdba1b
Erne <edainesi@fi.uba.ar>
2021-12-31 16:42:25 -0300

    add hidden option

--- lua/erne/options.lua
+++ lua/erne/options.lua
@@ -24,6 +24,7 @@ local options = {
     scrolloff = 8,                           -- is one of my fav
     sidescrolloff = 8,
     guifont = "monospace:h17",               -- the font used in graphical neovim applications
+    hidden = true,
 }
 
 for k, v in pairs(options) do
